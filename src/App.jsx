5cc97c15e5a61f46aac2999e5d07519a466db215
Sibusiso Ndlovu <sibusiso@jaspa.co.za>
2025-11-23 12:21:30 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     
+      <h1>Azure App Service Deployment</h1>
+     
+      <p >
+        This is just the test of React Azure App Service Deployment
+      </p>
+    </>
+  )
+}
+
+export default App
